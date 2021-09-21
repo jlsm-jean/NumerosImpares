005c27720fa04aa3c1f1cb4e7f677e88466bdda1
Jean Leonardo Soares de Moraes <jean.jlsm2@gmail.com>
2021-09-21 04:19:22 -0300

    Add project files.

---
+++ NúmerosImpares/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace NumerosImpares
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int X, i;
+
+            X = int.Parse(Console.ReadLine());
+            i = 1;
+
+            while (i <= X)
+            {
+                if (i % 2 != 0)
+                    Console.WriteLine(i);
+                i++;
+            }
+             
+                                
+        }
+
+     }
+}
+    
+
